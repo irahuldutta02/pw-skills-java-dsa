f221643de9eb89056741afbe20a86c7f0e01f20f
Rahul Dutta <rdtech2002@gmail.com>
2022-11-14 21:01:58 +0530

    ..

--- self-practice/src/p4.java
+++ self-practice/src/p4.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
+//if else statement
+//odd even
 public class p4{
     public static void main(String[] args) {
-    
+        System.out.print("Input a number to check even or odd : ");
+        Scanner sc = new Scanner (System.in);
+        int num = sc.nextInt();
+
+        if(num % 2 == 0 ){
+            System.out.println("The number is even.");
+        }
+        else{
+            System.out.println("The number is odd.");
+        }
     }
 }
