691bdbba5fb8846f2d9daf6664f70cca30c1eece
Rahul Dutta <rdtech2002@gmail.com>
2022-11-14 23:29:15 +0530

    .

--- self-practice/src/p6.java
+++ self-practice/src/p6.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
+//if else statement
+//child or teen or adult
 public class p6 {
     public static void main(String[] args) {
+        System.out.print("Your age : ");
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
 
+        if (age < 12){
+            System.out.println("Child");
+        } else if (age > 12 && age <18) {
+            System.out.println("Teen");
+        }else {
+            System.out.println("Adult");
+        }
+        sc.close();
     }
 }
