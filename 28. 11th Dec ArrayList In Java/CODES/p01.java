fac6e05f00ec19fde34cd8591c0bcbe9acd9cdc0
rahul <rdtech2002@gmail.com>
2023-01-11 16:19:10 +0530

    .

---
+++ 28. 11th Dec ArrayList In Java/CODES/p01.java
@@ -0,0 +1,9 @@
+public class p01{
+    public static void main(String[] args) {
+        Integer i = Integer.valueOf(4);
+        System.out.println(i); 
+        Float f = Float.valueOf(4.5f);
+        System.out.println(f);
+        
+    }
+} 
